f248bf5207d636e87cb546f917d2b3c65dc0a034
Charles Arruda Santos <charlestacodeouro@hotmail.com>
2025-05-19 20:56:21 +0200

    Arreglando proyecto

--- src/test/java/com/virosms/restaurantreservationchallenge/RestaurantReservationChallengeApplicationTests.java
+++ src/test/java/com/virosms/restaurantreservationchallenge/RestaurantReservationChallengeApplicationTests.java
@@ -2,8 +2,10 @@ package com.virosms.restaurantreservationchallenge;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RestaurantReservationChallengeApplicationTests {
 
     @Test
