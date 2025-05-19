f5cf548661cf35b9fd4d1c7d37ee87502ff7e4d0
Charles Arruda Santos <charlestacodeouro@hotmail.com>
2025-05-19 18:42:35 +0200

    First commit

---
+++ src/main/java/com/virosms/restaurantreservationchallenge/RestaurantReservationChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.virosms.restaurantreservationchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantReservationChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantReservationChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/virosms/restaurantreservationchallenge/RestaurantReservationChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virosms.restaurantreservationchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantReservationChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
